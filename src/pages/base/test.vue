5819acd838853037c00722d931ac2fdaca40e343
wei.yao <wei.yaoisnull@gmail.com>
2020-05-19 22:57:49 +0800

    分隔线

--- src/pages/base/test.vue
+++ src/pages/base/test.vue
@@ -10,6 +10,10 @@
     </div> -->
 
     {{$store.state.title}}===========>
+    <div class="split-line">我要做第一种分隔线</div>
+    <div class="split-line2"><span>
+        我要做第二种分隔线
+    </span></div>
     <ul>
         <tree v-for="(item,idx) in Products" :root="item" :depth=0 :key="idx">
             <template v-slot='slotProps'>
@@ -34,7 +38,7 @@
             // alert(store.state.title)
             return new Promise((resolve,reject)=>{
 
-            setTimeout(()=>{resolve('i am!!! ');console.log('i am coming...');},5000);
+            setTimeout(()=>{resolve('i am!!! ');console.log('i am coming...');},200);
             });
         },
         components: {
@@ -183,7 +187,59 @@
     };
 </script>
 
-<style type="text/css">
+<style type="text/css" lang="less">
+    .split-line2{
+        position: relative;
+        text-align: center;
+        line-height: 30px;
+        width:80%;
+        margin:0 auto;  
+        span{
+            background: #fff;
+            z-index: 3;
+            position:relative;
+            padding:0 30px;
+        }
+        &:before{
+            position:absolute;
+            content:' ';
+            top:50%;
+            left:0;
+            height:1px;
+            width:50%;
+            background: #f00;
+        }
+         &:after{
+            position:absolute;
+            content:' ';
+            top:50%;
+            right:0;
+            height:1px;
+            width:50%;
+            background: #f00;
+        }
+    }
+     .split-line{
+           display: flex;
+            line-height:32px;
+            border:1px solid #e9e9e9;
+            width:80%;
+            margin:0 auto;
+            align-items: center;
+        }
+        .split-line::before,.split-line::after{
+            content: ' ';
+            flex:1;
+            height:1px;
+            background: #f00;
+           
+        }
+        .split-line::after{
+             margin-left:30px;
+        }
+        .split-line::before{
+            margin-right:30px;
+        }
     .item{
         padding-left: 40px;
     }
